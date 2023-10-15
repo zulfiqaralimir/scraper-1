72f9fda841b20c6b5c1bb1c2166de2c9fff2b5f4
ZULFIQAR ALI MIR <68346772+zulfiqaralimir@users.noreply.github.com>
2023-10-15 23:24:38 +0500

    Update scraper-1.py

--- scraper-1.py
+++ scraper-1.py
@@ -3,16 +3,17 @@ import pandas as pd
 # URL of the webpage with the table
 url = 'https://www.psx.com.pk/market-summary/'
 tables = pd.read_html(url)
-df=len(tables)
+#df=len(tables)
 st.title("CEMENT Sector Market Summary")
+
 import plotly.express as px
 
 # Example Plotly chart
 fig = px.bar(tables[5], x='CEMENT', y='CEMENT.7', title='Table Data Volume')
 st.plotly_chart(fig)
-st.write("Number of Tables:")
-st.write(df)
-st.write(tables[5])
+#st.write("Number of Tables:")
+#st.write(df)
+#st.write(tables[5])
 
 
 
